d30cd4389b84cd164f08bc28876c89f7b788c875
Emil Enchev <emoen4ev@gmail.com>
2024-01-10 18:30:52 +0200

    Created index.view.php file.

--- index.php
+++ index.php
@@ -1,58 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
+<?php
 
-        }
-    </style>
-</head>
-<body>
-    <?php
-        $books = [
-            [
-                'name' => 'Do Androids dream of Electric Sheep',
-                'author' => 'Philip K. Dick',
-                'releaseYear' => 1968,
-                'purchaseUrl' => 'https://example.com'
-            ],
-            [
-                'name' => 'Project Hail Mary',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2021,
-                'purchaseUrl' => 'https://example.com'
-            ],
-            [
-                'name' => 'The Martian',
-                'author' => 'Andy Weir',
-                'releaseYear' => 2011,
-                'purchaseUrl' => 'https://example.com'
-            ]
+$books = [
+    [
+        'name' => 'Do Androids dream of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'https://example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'https://example.com'
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'https://example.com'
+    ]
 
-        ];
+];
 
-        $filteredBooks = array_filter($books, function ($book) {
-            return $book['releaseYear'] >= 2000;
-        });
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['releaseYear'] >= 2000;
+});
 
-    ?>
-
-    <ul>
-        <?php foreach ($filteredBooks as $book) : ?>
-            <li>
-                <a href="<?= $book['purchaseUrl']?>">
-                    <?= $book['name']; ?> (<?= $book['releaseYear']?>) - By <?= $book['author'] ?>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-
-</body>
-</html>
+require "index.view.php";
