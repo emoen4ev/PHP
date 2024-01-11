6d28a3db99ab449b3ab462595cf78995b519867e
Emil Enchev <emoen4ev@gmail.com>
2024-01-11 15:53:51 +0200

    Episode 12, Page Links, Exercises.

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
 <?php
 
-$books = [
-    [
-        'name' => 'Do Androids dream of Electric Sheep',
-        'author' => 'Philip K. Dick',
-        'releaseYear' => 1968,
-        'purchaseUrl' => 'https://example.com'
-    ],
-    [
-        'name' => 'Project Hail Mary',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2021,
-        'purchaseUrl' => 'https://example.com'
-    ],
-    [
-        'name' => 'The Martian',
-        'author' => 'Andy Weir',
-        'releaseYear' => 2011,
-        'purchaseUrl' => 'https://example.com'
-    ]
 
-];
-
-$filteredBooks = array_filter($books, function ($book) {
-    return $book['releaseYear'] >= 2000;
-});
 
 require "index.view.php";
