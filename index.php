8f9c0bab28cc8fd7e1afc8930faec8bff826b1a9
Emil Enchev <emoen4ev@gmail.com>
2024-01-11 20:13:19 +0200

    Episode 14, Superglobals and Current Page Styling, Exercises, Part 1.

--- index.php
+++ index.php
@@ -2,4 +2,21 @@
 
 $heading = 'Home';
 
+echo $_SERVER["REQUEST_URI"];
+
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+if ($_SERVER['REQUEST_URI'] === '/') {
+    echo 'bg-gray-900 text-white';
+} else {
+    echo 'text-gray-300';
+}
+
 require "views/index.view.php";
