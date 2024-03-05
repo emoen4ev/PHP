bb025f4554d3357d6d8b76c53f7bad6c5594259d
Emil Enchev <emoen4ev@gmail.com>
2024-03-05 19:21:14 +0200

    Episode 22, Render the Notes and Note Page, part 1.

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
 
-// use JetBrains\PhpStorm\NoReturn;
-
 require 'functions.php';
-
 require 'Database.php';
-
-$config = require 'config.php';
-
-$db = new Database($config['database']);
-
-$id = $_GET['id'];
-
-$query = "select * from  posts where id = :id";
-
-$posts = $db->query($query, [':id' => $id])->fetch();
-
-dd($posts);
+require 'router.php';
