a2d7d933825115ce2771dbd7647885de4695a064
Emil Enchev <emoen4ev@gmail.com>
2024-03-07 18:21:53 +0200

    Episode 22, Render the Notes and Note Page, part 2.

--- views/notes.view.php
+++ views/notes.view.php
@@ -5,7 +5,11 @@
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
         <?php foreach ($notes as $note) : ?>
-            <li><?= $note['body'] ?></li>
+            <li>
+                <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+                    <?= $note['body'] ?>
+                </a>
+            </li>
         <?php endforeach; ?>
     </div>
 </main>
